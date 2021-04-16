8e97289cdcc2dbec6c8be102fa7e3531057bec63
Mohammad Ihraiz <mohammad.a.ihraiz@gmail.com>
2021-04-16 21:37:27 +0300

    V 1.0

---
+++ app/src/main/java/edu/birzeit/stopwatch/MainActivity.java
@@ -0,0 +1,54 @@
+package edu.birzeit.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        runTimer();
+    }
+
+    private void runTimer() {
+        final TextView txtTime = findViewById(R.id.txtClock);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (running) {
+                    seconds++;
+                }
+                int hours = seconds/3600;
+                int minutes = (seconds%3600) / 60;
+                int secs = seconds%60;
+                String time = String.format("%02d", hours) + ":" + String.format("%02d", minutes) + ":" + String.format("%02d", secs);
+                txtTime.setText(time);
+                handler.postDelayed(this,1000);
+            }
+        } );
+
+    }
+
+    public void btnResetOnClick(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    public void btnStopOnClick(View view) {
+        running = false;
+    }
+
+    public void btnStartOnClick(View view) {
+        running = true;
+    }
+}
