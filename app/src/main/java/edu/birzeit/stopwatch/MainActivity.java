0207aa3f9a24b83dc0fda83ca1f9fa1a114c9670
Mohammad Ihraiz <mohammad.a.ihraiz@gmail.com>
2021-04-16 21:55:48 +0300

    V 1.1
    Save state when change orientation

--- app/src/main/java/edu/birzeit/stopwatch/MainActivity.java
+++ app/src/main/java/edu/birzeit/stopwatch/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.birzeit.stopwatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,11 +13,30 @@ public class MainActivity extends AppCompatActivity {
     private int seconds = 0;
     private boolean running = false;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkInstance(savedInstanceState);
         runTimer();
+
+    }
+
+    private void checkInstance(Bundle savedInstanceState) {
+        if(savedInstanceState != null){
+            seconds = savedInstanceState.getInt("SECONDS");
+            running = savedInstanceState.getBoolean("RUNNING");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("SECONDS", seconds);
+        outState.putBoolean("RUNNING", running);
+
     }
 
     private void runTimer() {
@@ -27,12 +47,13 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 if (running) {
                     seconds++;
+                    int hours = seconds/3600;
+                    int minutes = (seconds%3600) / 60;
+                    int secs = seconds%60;
+                    String time = String.format("%02d", hours) + ":" + String.format("%02d", minutes) + ":" + String.format("%02d", secs);
+                    txtTime.setText(time);
                 }
-                int hours = seconds/3600;
-                int minutes = (seconds%3600) / 60;
-                int secs = seconds%60;
-                String time = String.format("%02d", hours) + ":" + String.format("%02d", minutes) + ":" + String.format("%02d", secs);
-                txtTime.setText(time);
+
                 handler.postDelayed(this,1000);
             }
         } );
